5f69c1a44f52ac65541e770fcaa791fb3ea3a7f4
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-07-27 15:18:32 -0400

    create page layout to follow screenshot from the lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,32 @@
 export default function App() {
-  return <div>Hello World!</div>;
+  return (
+    <main>
+      <h1>Todo List</h1>
+
+      <div>
+        <input type="text" placeholder="Enter a new todo" />
+        <button type="button">Add Task </button>
+      </div>
+
+      <ul>
+
+        <input type="checkbox" />
+        <span>Create Mockup</span>
+        <button type="button">Edit </button>
+        <button type="button">Delete </button>
+
+        <br></br>
+
+        <input type="checkbox" />
+        <span>Create Static Layout </span>
+        <button type="button">Edit </button>
+        <button type="button">Delete </button>
+        <br></br>
+        <input type="checkbox" />
+        <input type="text" placeholder="Add Interactive" />
+        <button type="button">Add Interactive</button>
+
+      </ul>
+    </main>
+  );
 }
